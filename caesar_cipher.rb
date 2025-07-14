9e162c5e32a01a3dcb2fb2ba840b86d8984fbb23
Henry Mandorla <henry.mandorla@gmail.com>
2025-07-14 14:00:40 +1000

    RuboCop

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,19 +1,21 @@
+# frozen_string_literal: true
+
 def caesar_cipher(string, number)
-    return nil if string == ""
-    alphabet = ('a'..'z').to_a
-    string_lower = string.downcase
-    char_array = string_lower.split("")
-    new_chars = char_array.map do |char|
-        if alphabet.include?(char)
-            index = alphabet.index(char)
-            shifted_char = alphabet[(index - number) % 26]
-            shifted_char
-        else
-            char
-        end
+  return nil if string == ''
+
+  alphabet = ('a'..'z').to_a
+  string_lower = string.downcase
+  char_array = string_lower.split('')
+  new_chars = char_array.map do |char|
+    if alphabet.include?(char)
+      index = alphabet.index(char)
+      shifted_char = alphabet[(index - number) % 26]
+      shifted_char
+    else
+      char
     end
-    new_string = new_chars.join("")
-    return new_string
+  end
+  new_chars.join('')
 end
 
 puts caesar_cipher('cowpoke', 5)
